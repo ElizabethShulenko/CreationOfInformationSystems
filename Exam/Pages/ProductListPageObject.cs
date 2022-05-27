de3195e12ab0fc72fcdde3782082c3c0737601dd
Yelyzaveta Shulenko <liza.shulenko@gmail.com>
2022-05-27 13:07:49 +0300

    Exam

---
+++ Exam/Pages/ProductListPageObject.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Exam.Pages
+{
+    class ProductListPageObject
+    {
+        private IWebDriver _webDriver;
+
+        #region Locators
+
+        #endregion
+
+        #region WebElements
+        [FindsBy(How = How.XPath, Using = ".//div[contains(@class, 'slider-filter')]/input[@formcontrolname='min']")]
+        public IWebElement LowerPrice { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//div[contains(@class, 'slider-filter')]/input[@formcontrolname='max']")]
+        public IWebElement UpperPrice { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//div[contains(@class,'slider-filter')]/button")]
+        public IWebElement FilterPriceButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//rz-catalog-tile")]
+        public IList<IWebElement> ProductNodes { get; set; }
+
+        #endregion
+
+        public ProductListPageObject(IWebDriver driver)
+        {
+            _webDriver = driver;
+
+            PageFactory.InitElements(_webDriver, this);
+        }
+
+        public void FilterByPrice(decimal minPrice, decimal maxPrice)
+        {
+            LowerPrice.Clear();
+            LowerPrice.SendKeys(minPrice.ToString());
+
+            UpperPrice.Clear();
+            UpperPrice.SendKeys(maxPrice.ToString());
+
+            FilterPriceButton.Click();
+
+            Thread.Sleep(2000);
+        }
+
+        public static bool CheckProductsInPriceRange(IEnumerable<IWebElement> products, decimal minPrice, decimal maxPrice)
+        {
+            var productPriceLocator = By.XPath(".//span[contains(@class,'price-value')]");
+
+            foreach(var product in products)
+            {
+                var priceString = product.FindElement(productPriceLocator).Text;
+
+                priceString = priceString
+                    .Replace(" ", "")
+                    .Replace("₴", "");
+
+                var price = Decimal.Parse(priceString);
+
+                if(price < minPrice || price > maxPrice)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Exam/Tests/ProductListPageObjectTests.cs
@@ -0,0 +1,64 @@
+﻿using Exam.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Exam.Tests
+{
+    [TestFixture]
+    public class ProductListPageObjectTests
+    {
+        private IWebDriver _webDriver;
+
+        private readonly string _uri = "https://rozetka.com.ua/notebooks/c80004/";
+
+        #region Startup
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _webDriver = new ChromeDriver();
+
+            _webDriver.Manage().Window.Maximize();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _webDriver.Navigate().GoToUrl(_uri);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _webDriver.Quit();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Manage().Cookies.DeleteAllCookies();
+        }
+        #endregion
+
+        [Test]
+        public void GetProducts()
+        {
+            //arrange
+            var productListPageObject = new ProductListPageObject(_webDriver);
+
+            var minPrice = 3000;
+            var maxPrice = 20000;
+
+            //act
+            productListPageObject.FilterByPrice(minPrice, maxPrice);
+
+            var products = productListPageObject.ProductNodes;
+
+            var result = ProductListPageObject.CheckProductsInPriceRange(products, minPrice, maxPrice);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+    }
+}
